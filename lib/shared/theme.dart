5d3a0438bf76ea92f211a79a7f80e36ea1249df2
Loviana Mayoreta <lovianamayoreta@gmail.com>
2021-11-13 11:26:49 +0700

    add shared theme colors

---
+++ lib/shared/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff5C40CC);
+const Color kBlackColor = Color(0xff1F1449);
+const Color kWhiteColor = Color(0xffFFFFFF);
+const Color kOutlineColor = Color(0xffDBD7EC);
+const Color kGreyColor = Color(0xff9698A9);
+const Color kGreenColor = Color(0xff0EC3AE);
+const Color kRedColor = Color(0xffEB70A5);
+const Color kBackgroundColor = Color(0xffFAFAFA);
+const Color kOrangeColor = Color(0xffFFA235);
